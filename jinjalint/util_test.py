58bc8856020b3b1181fcfc0d439220c2fd21e9b1
Antoine Motet <antoine.motet@gmail.com>
2018-12-24 14:02:54 +0100

    Add more tests for `flatten`

--- jinjalint/util_test.py
+++ jinjalint/util_test.py
@@ -2,4 +2,7 @@ from .util import flatten
 
 
 def test():
-    assert list(flatten([2, [], [3, [[4], 5]]])) == [2, 3, 4, 5]
+    assert list(flatten(())) == []
+    assert list(flatten([])) == []
+    assert list(flatten((1,))) == [1]
+    assert list(flatten([2, [], (), [3, [(4, 5), (6,)]]])) == [2, 3, 4, 5, 6]
