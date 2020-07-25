5d23ba49b837e38ac147c17ff2e3648c011ae9ce
Thibaud Colas <thibaudcolas@gmail.com>
2020-07-26 00:31:08 +0100

    Add missing integration of new rules

--- curlylint/check.py
+++ curlylint/check.py
@@ -8,8 +8,12 @@ from curlylint.rules.django_forms_rendering.django_forms_rendering import (
 )
 from curlylint.rules.html_has_lang.html_has_lang import html_has_lang
 from curlylint.rules.image_alt.image_alt import image_alt
-from curlylint.rules.indent.indent import INDENT, indent
+from curlylint.rules.indent.indent import indent
+from curlylint.rules.meta_viewport.meta_viewport import meta_viewport
 from curlylint.rules.no_autofocus.no_autofocus import no_autofocus
+from curlylint.rules.tabindex_no_positive.tabindex_no_positive import (
+    tabindex_no_positive,
+)
 from curlylint.util import flatten
 
 err = partial(click.secho, fg="red", err=True)
@@ -19,8 +23,10 @@ checks = {
     "django_forms_rendering": django_forms_rendering,
     "html_has_lang": html_has_lang,
     "image_alt": image_alt,
+    "meta_viewport": meta_viewport,
     "no_autofocus": no_autofocus,
-    INDENT: indent,
+    "tabindex_no_positive": tabindex_no_positive,
+    "indent": indent,
 }
 
 
