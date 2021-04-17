7a081706e70480e23ea27c567cbc25121489938e
Thibaud Colas <thibaudcolas@gmail.com>
2021-04-17 15:09:46 +0100

    Add more basic CLI tests

--- curlylint/cli_test.py
+++ curlylint/cli_test.py
@@ -1,11 +1,52 @@
 import unittest
 
+from io import BytesIO
+
 from curlylint.tests.utils import BlackRunner
 
 from curlylint.cli import main
 
 
 class TestParser(unittest.TestCase):
+    def test_no_flag(self):
+        runner = BlackRunner()
+        result = runner.invoke(main, [])
+        self.assertEqual(runner.stdout_bytes.decode(), "")
+        self.assertEqual(
+            runner.stderr_bytes.decode(), "No Path provided. Nothing to do 😴\n"
+        )
+        self.assertEqual(result.exit_code, 0)
+
+    def test_stdin(self):
+        runner = BlackRunner()
+        result = runner.invoke(
+            main, ["-"], input=BytesIO("<p>Hello, World!</p>".encode("utf8")),
+        )
+        self.assertEqual(runner.stdout_bytes.decode(), "")
+        self.assertEqual(runner.stderr_bytes.decode(), "All done! ✨ 🍰 ✨\n\n")
+        self.assertEqual(result.exit_code, 0)
+
+    def test_stdin_verbose(self):
+        runner = BlackRunner()
+        result = runner.invoke(
+            main,
+            ["--verbose", "-"],
+            input=BytesIO("<p>Hello, World!</p>".encode("utf8")),
+        )
+        self.assertEqual(runner.stdout_bytes.decode(), "")
+        self.assertIn(
+            "Identified project root as:", runner.stderr_bytes.decode()
+        )
+        self.assertIn(
+            """Analyzing file content from stdin
+Files being analyzed:
+-
+All done! ✨ 🍰 ✨
+""",
+            runner.stderr_bytes.decode(),
+        )
+        self.assertEqual(result.exit_code, 0)
+
     def test_flag_help(self):
         runner = BlackRunner()
         result = runner.invoke(main, ["--help"])
